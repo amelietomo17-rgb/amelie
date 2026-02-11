755a9e27bc94e52292b2157ffb1c8280a43144c1
amelietomo17-rgb <amelietomo17@gmail.com>
2026-02-11 09:20:26 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,28 @@
+let angle = 0;
+
 function setup() {
-  createCanvas(620, 420);
- }
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
 
 function draw() {
-  background(220);
-  for (let i = 0; i<15; i += 1) {
-    for (let j = 0; j<10; j += 1) {
-      if (i!==int((mouseX-10)/40) || j!==int((mouseY-10)/40))
-          {
-          couleur = color(0,255,0);
-          fill(couleur);
-          rect(40*i+10,40*j+10,40,40);
-          line( 40*i+20,40*j+20,40*i+40,40*j+40);
-          line( 40*i+40,40*j+20,40*i+20,40*j+40);
-          }
-      else if (j<5) {couleur = color(255,255,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            fill(color(0,0,250));
-            rect(40*i+20,40*j+20,20,20);}
-      else if (i<8) {couleur = color(255,255,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            line( 40*i+20,40*j+20,40*i+40,40*j+40);
-            line( 40*i+30,40*j+20,40*i+40,40*j+30);
-            line( 40*i+20,40*j+20,40*i+40,40*j+20);
-            line( 40*i+40,40*j+20,40*i+40,40*j+40);
-            }
-        else
-          {
-            couleur = color(255,0,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            fill(color(0,0,250));
-            circle(40*i+30,40*j+30,20);
-          }
-          }
-      }
-      
-     
+  background(255);
+
+  translate(width / 2, height / 2);
+  rotate(angle);
+
+  stroke(0);          
+  strokeWeight(8);    
+  strokeCap(ROUND);  
+  noFill();
+
+  let size = 100;
+
+  for (let i = 0; i < 8; i++) {
+    line(0, -size, 0, size);
+    rotate(45); 
+  }
+
+  angle += 1; 
 }
+
